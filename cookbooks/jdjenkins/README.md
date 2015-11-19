0da6a17b98143f60672cc44d6fef3bb13c44bc30
JD Goins <jameson.goins@agilent.com>
2015-11-19 10:05:45 -0800

    added jdjenkins cookbook

---
+++ cookbooks/jdjenkins/README.md
@@ -0,0 +1,4 @@
+# jdjenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/jdjenkins/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: jdjenkins
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+include_recipe 'jenkins::master'
+
