7339c7b36e573348f68ceb8715ff63c4e7b670b4
JD Goins <jameson.goins@agilent.com>
2015-11-19 13:27:18 -0800

    added docker and chefdk

--- cookbooks/jdjenkins/recipes/default.rb
+++ cookbooks/jdjenkins/recipes/default.rb
@@ -5,4 +5,28 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'jenkins::master'
+include_recipe 'chef-dk'
+
+package 'git' do
+  action :install
+end
+
+jenkins_plugin 'git'
+
+chef_dk 'my_chef_dk'
+
+group 'docker' do
+  action :create
+  gid 999
+  members ['ec2-user']
+end
+
+package 'docker-io' do
+  action :install
+end
+
+service 'docker' do
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+end
 
