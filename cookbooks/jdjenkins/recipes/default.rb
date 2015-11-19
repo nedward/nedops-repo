d74b43f1f2e745ab58ccfa24e8c35b694e9ca28c
JD Goins <jameson.goins@agilent.com>
2015-11-19 15:04:29 -0800

    dffg

--- cookbooks/jdjenkins/recipes/default.rb
+++ cookbooks/jdjenkins/recipes/default.rb
@@ -4,18 +4,6 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-user 'jenkins' do
-  action :remove
-  # comment 'Jenkins Service User'
-  # uid 1000
-  # gid 'jenkins'
-  # home '/home/jenkins'
-  # shell '/bin/zsh'
-  # password 'Agilent123'
-  # supports :manage_home => true
-end
-
-
 include_recipe 'jenkins::master'
 include_recipe 'chef-dk'
 
@@ -23,11 +11,14 @@ package 'git' do
   action :install
 end
 
-jenkins_plugin 'git'
+jenkins_plugin 'git' do
+	notifies :restart, 'service[jenkins]'
+end
 
-chef_dk 'my_chef_dk'
-
-
+chef_dk 'my_chef_dk' do
+	global_shell_init true
+	action :install
+end
 
 group 'docker' do
   action :create
@@ -39,8 +30,18 @@ package 'docker-io' do
   action :install
 end
 
+gem_package "kitchen-docker" do
+  gem_binary "/opt/chefdk/embedded/bin/gem"
+  options "--no-user-install"
+  notifies :restart, "service[docker]"
+end
+
 service 'docker' do
   supports :status => true, :restart => true, :reload => true
   action [:start, :enable]
 end
 
+service 'jenkins' do
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable, :restart]
+end
